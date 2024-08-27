30923abbac5c3cbdf6ca42eb29b6bdca4c7d19fc
Jakub Zygmunt <jakub.zygmunt.afm@gmail.com>
2024-08-27 21:23:20 +0200

    Collect all show names and platform id

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 mKino is a simple extension that allows you check show ratings and reviews
 
 ## MVP
-[x] Show ratings on netflix shows
-[ ] Collect all show names and platform id
+[x] Show ratings on netflix shows (without hover)
+[ ] Show ratings on netflix shows (with hover)
+[x] Collect all show names and platform id
 [ ] Mock http server with rating responses
 [ ] Collect real data
 [ ] Add addon UI
--- chrome_extension/scripts/content.js
+++ chrome_extension/scripts/content.js
@@ -1,3 +1,5 @@
+const DELAY = 500;
+
 function createRatingContainer() {
   const ratingContainer = document.createElement("div");
   ratingContainer.classList.add("mk-rating-container");
@@ -8,7 +10,7 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-const getAllCards = (retry = 0) => {
+function getAllCards(retry = 0) {
   return new Promise(async (resolve) => {
     if (retry > 5) {
       return resolve([]);
@@ -20,17 +22,17 @@ const getAllCards = (retry = 0) => {
     }
     return resolve(cards);
   });
-};
+}
 
-const updateRating = (cards) => {
+async function updateRating({ cards, ratings }) {
   for (let i = 0; i < cards.length; i++) {
     const card = cards[i];
     if (card.querySelector(".mk-rating-container")) {
       continue;
     }
-    console.log("adding rating to card", card);
+    const { value } = ratings.get(card);
     const ratingContainer = createRatingContainer();
-    ratingContainer.innerHTML = "6/10 ⭐️";
+    ratingContainer.innerHTML = `${value}/10 ⭐️`;
     ratingContainer.style.position = "absolute";
     ratingContainer.style.top = "10px";
     ratingContainer.style.right = "10px";
@@ -40,15 +42,67 @@ const updateRating = (cards) => {
     ratingContainer.style.borderRadius = "5px";
     ratingContainer.style.fontSize = "12px";
     ratingContainer.style.fontWeight = "bold";
+    ratingContainer.style.transition = "opacity 1s";
+    ratingContainer.style.opacity = 0;
+    setTimeout(() => {
+      ratingContainer.style.opacity = 1;
+    }, DELAY);
 
     card.appendChild(ratingContainer);
   }
-};
+}
 
-getAllCards().then(updateRating);
+const data = new Map();
 
-const observer = new MutationObserver((mutations) => {
-  getAllCards().then(updateRating);
+async function collectDataFromCards(cards) {
+  for (let i = 0; i < cards.length; i++) {
+    const card = cards[i];
+    if (data.has(card)) continue;
+    const title = card.getElementsByClassName("fallback-text")[0].innerText;
+    const id = card
+      .getElementsByTagName("a")[0]
+      .href.split("/")[4]
+      .split("?")[0];
+    data.set(card, { title, id });
+  }
+
+  return { cards, data };
+}
+
+const ratings = new Map();
+const queue = new Set();
+let timeoutId = null;
+
+async function fetchRatings({ cards, data }) {
+  return new Promise((resolve) => {
+    // filter already fetched ratings
+    for (let i = 0; i < cards.length; i++) {
+      const card = cards[i];
+      if (ratings.has(card)) {
+        continue;
+      }
+      queue.add(card);
+
+      // clear pending timeout
+      if (timeoutId) clearTimeout(timeoutId);
+
+      // create new timeout window
+      timeoutId = setTimeout(() => {
+        for (const c of queue) {
+          ratings.set(c, { value: Math.round(Math.random() * 10) });
+          queue.delete(c);
+        }
+        resolve({ cards, data, ratings });
+      }, DELAY);
+    }
+  });
+}
+
+const observer = new MutationObserver(() => {
+  getAllCards()
+    .then(collectDataFromCards)
+    .then(fetchRatings)
+    .then(updateRating);
 });
 
 observer.observe(document.body, {
